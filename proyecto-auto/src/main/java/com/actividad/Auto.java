aa4b1c67ebd946d845caf789ac7b080f218bbe30
ciroescuderobrayan-sdj <ciroescuderobrayan@gmail.com>
2026-02-09 15:11:05 -0500

    Termine parte logica

--- proyecto-auto/src/main/java/com/actividad/Auto.java
+++ proyecto-auto/src/main/java/com/actividad/Auto.java
@@ -2,9 +2,41 @@ package com.actividad;
 
 public class Auto {
 
-public String marca;
-public String modelo;
-public String color;
-public int velocidad; 
+    // Atributos
+    String marca;
+    String modelo;
+    String color;
+    int velocidad;
+
+    // Metodo para encender el auto
+    public void encender() {
+        System.out.println("El auto esta encendido");
+    }
+
+    // Metodo para acelerar el auto
+    public void acelerar(int aumento) {
+        velocidad += aumento;
+        System.out.println("El auto acelero a " + velocidad + " km/h");
+    }
+
+    // Metodo para frenar el auto
+    public void frenar(int reduccion) {
+        velocidad -= reduccion;
+        if (velocidad < 0) {
+            velocidad = 0;
+        }
+        System.out.println("El auto frena. velocidad actual " + velocidad + " km/h");
+
+    }
+
+    // Metodo para mostrar información//
+
+    public void mostrarInformación() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Color: " + color);
+        System.out.println("Velocidad: " + velocidad + " km/h");
+        System.out.println("-----------------------------");
+    }
 
 }
--- proyecto-auto/src/main/java/com/actividad/Main.java
+++ proyecto-auto/src/main/java/com/actividad/Main.java
@@ -2,6 +2,33 @@ package com.actividad;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // primer auto
+        Auto auto1 = new Auto();
+        auto1.marca = "Toyota";
+        auto1.modelo = "Corolla";
+        auto1.color = "Rojo";
+        auto1.velocidad = 0;
+
+        // segundo auto
+        Auto auto2 = new Auto();
+        auto2.marca = "Chevrolet";
+        auto2.modelo = "spark";
+        auto2.color = "Blanco";
+        auto2.velocidad = 0;
+
+        // Acciones auto 1
+        auto1.encender();
+        auto1.acelerar(60);
+        auto1.frenar(20);
+
+        // Acciones auto 2
+        auto2.encender();
+        auto2.acelerar(40);
+
+        // Mostrar información de ambos autos
+        auto1.mostrarInformación();
+        auto2.mostrarInformación();
+
     }
 }
